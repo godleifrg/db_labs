fc25c4bf036d82999621f3655bf987f34f2372ae
Gleb <poprotskiy04@gmail.com>
2023-11-07 10:31:10 +0400

    Edited 3

--- lab_2/task3.sql
+++ lab_2/task3.sql
@@ -1,4 +1,4 @@
 /*Выбрать объекты, пользование которых бесплатно для членов клуба.
 */
 use cd;
-SELECT * FROM facilities WHERE membercost = 0;
+SELECT facility FROM facilities WHERE membercost = 0;
