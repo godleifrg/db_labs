2ceafa130b051d764b1ef1d78fb380aee01a8079
Gleb <poprotskiy04@gmail.com>
2023-11-21 11:54:04 +0400

    fixed 4

--- lab_3/task3_4.sql
+++ lab_3/task3_4.sql
@@ -1,7 +1,6 @@
 /*Выберите всех членов клуба и членов, которые их рекомендовали,  отсортировав их по имени и фамилии.*/
 USE cd;
-SELECT
-DISTINCT CONCAT(mem1.surname, ' ', mem1.firstname) AS 'Участник',
+SELECT CONCAT(mem1.surname, ' ', mem1.firstname) AS 'Участник',
 CONCAT(mem2.surname, ' ', mem2.firstname) AS 'Рекомендован'
 FROM members as mem1
 LEFT JOIN members AS mem2 ON mem1.recommendedby = mem2.memid
